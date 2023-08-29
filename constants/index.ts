216a73b5af54ecfdd1d1d155940ab82ac138ded0
vg18gupta <vg.vishal.gupta.10@gmail.com>
2023-08-29 23:08:35 +0530

    New post added

---
+++ constants/index.ts
@@ -0,0 +1,15 @@
+import thumbPortfolio from '../public/images/contents/youtube-how-to-build-portfolio.jpg';
+import linkDrop from '../public/images/contents/youtube-how-to-use-inkdrop.jpg';
+
+export const postContent = [
+  {
+    title: 'Using JavaScript for Safe Cross-Domain Communication',
+    thumb: thumbPortfolio,
+    href: 'https://medium.com/@vg.vishal.gupta.10/using-javascript-and-window-postmessage-for-safe-cross-domain-communication-71651da4d1d9'
+  },
+  {
+    title: 'What is React Server Component and How to use',
+    thumb: linkDrop,
+    href: 'https://medium.com/@vg.vishal.gupta.10/what-is-react-server-component-and-how-to-use-c84325d52676'
+  }
+]
--- pages/posts.tsx
+++ pages/posts.tsx
@@ -3,7 +3,8 @@ import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import { GridItem } from '../components/grid-item'
 
-import thumbPortfolio from '../public/images/contents/youtube-how-to-build-portfolio.jpg'
+import { postContent } from '../constants/index';
+
 const Posts = () => (
   <Layout title="Posts">
     <Container>
@@ -13,12 +14,13 @@ const Posts = () => (
 
       <Section delay={0.1}>
         <SimpleGrid columns={[1, 2, 2]} gap={6}>
-          <GridItem
-            title="Using JavaScript for Safe Cross-Domain Communication"
-            thumbnail={thumbPortfolio}
-            
-            href="https://medium.com/@vg.vishal.gupta.10/using-javascript-and-window-postmessage-for-safe-cross-domain-communication-71651da4d1d9"
-          />
+          {postContent.map((item) => (
+            <GridItem
+              title={item.title}
+              thumbnail={item.thumb}
+              href={item.href}
+            />
+          ))}
         </SimpleGrid>
       </Section>
     </Container>
